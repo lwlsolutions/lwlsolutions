f737261137232e367d3c13c70cf94cb1ed6a650d
DevAppsAagro/pecuaria.app <devapp.apps@gmail.com>
2025-04-24 12:13:25 -0300

    Atualiza next.config.js com configurações para resolver erro 404 no Vercel

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     domains: [], // Adicione domínios externos se usar imagens externas
+    unoptimized: true,
   },
+  trailingSlash: false,
 };
 
 module.exports = nextConfig;
